6e1bc21244e8d12a45d09389c7cd170429b9d6a4
Jean-philippe Martel <jemartel@c2r5p3.42quebec.com>
2021-03-16 19:25:48 -0400

    addedexo6

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_lowercase.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jean-phil <jemartel@student.42quebec>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/16 16:53:51 by jean-phil         #+#    #+#             */
-/*   Updated: 2021/03/16 19:24:28 by jean-phil        ###   ########.fr       */
+/*   Created: 2021/03/16 19:13:30 by jean-phil         #+#    #+#             */
+/*   Updated: 2021/03/16 19:22:29 by jean-phil        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_lowercase (char *str)
+int	ft_str_printable(char *input)
 {
-	int		output;
+	int		value;
 
-	while (*str != '\0')
+	value = 1;
+	while (*input != '\0')
 	{
-		output = 1;
-		if (*str >= 97 && *str <= 122)
-			output = 0;
-		str++;
-	}
-	return (output);
+		value = 1;
+		if (*input >= 0 && *input <= 127)
+			 	input = 0;
+	}	
+	return (value);
 }
