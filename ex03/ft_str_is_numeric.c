1ef91d0ab3cbb9310d3385b50986f9e9059bad36
Jean-philippe Martel <jemartel@c2r5p3.42quebec.com>
2021-03-16 16:08:38 -0400

    prelinting

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jean-phil <jemartel@student.42quebec>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 15:32:13 by jean-phil         #+#    #+#             */
+/*   Updated: 2021/03/16 16:07:23 by jean-phil        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	str_lengeh ( char *str)
+{
+	char	*ray;
+	int		lenght;
+
+	ray = str;
+	lenght = 0;
+	 	while (*ray != 0)
+	{
+		ray++;
+		lenght++;
+	}
+	return (lenght);
+}
+
+int	ft_str_is_numeric(char *str)
+{
+	int		iterator;
+	int		value;	
+	int		output;
+	output = 1;
+	value = 0;	
+
+	iterator = 0;
+	value = str_lengeh(str);
+	while ( iterator != value)
+	{
+		iterator++;
+	if(*str < 58)
+		output= 0;
+
+	}
+	 return (output);
+}
+
